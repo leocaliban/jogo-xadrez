62e02b1a1c326e9bc836a449d0f51109aa1dbf10
Leocaliban <leocaliban@gmail.com>
2019-06-24 17:12:02 -0300

    Cria classes para Tabuleiro, Peça e Posição

--- src/com/leocaliban/jogo/xadrez/application/Main.java
+++ src/com/leocaliban/jogo/xadrez/application/Main.java
@@ -1,10 +1,15 @@
 package com.leocaliban.jogo.xadrez.application;
 
+import com.leocaliban.jogo.xadrez.tabuleiro.Posicao;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("JOGO DE XADREZ");
 
+		Posicao posicao = new Posicao(1, 2);
+
+		System.out.println(posicao);
 	}
 
 }
---
+++ src/com/leocaliban/jogo/xadrez/tabuleiro/Peca.java
@@ -0,0 +1,16 @@
+package com.leocaliban.jogo.xadrez.tabuleiro;
+
+public class Peca {
+
+	protected Posicao posicao;
+	private Tabuleiro tabuleiro;
+
+	public Peca(Tabuleiro tabuleiro) {
+		this.tabuleiro = tabuleiro;
+	}
+
+	protected Tabuleiro getTabuleiro() {
+		return tabuleiro;
+	}
+
+}
