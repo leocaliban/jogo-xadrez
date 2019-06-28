02089102d8326ba9d033c7ca9d0d795877be391a
Leocaliban <leocaliban@gmail.com>
2019-06-28 20:39:05 -0300

    Implementa regras de movimentação do Rei

--- src/com/leocaliban/jogo/xadrez/xadrez/pecas/Rei.java
+++ src/com/leocaliban/jogo/xadrez/xadrez/pecas/Rei.java
@@ -1,5 +1,6 @@
 package com.leocaliban.jogo.xadrez.xadrez.pecas;
 
+import com.leocaliban.jogo.xadrez.tabuleiro.Posicao;
 import com.leocaliban.jogo.xadrez.tabuleiro.Tabuleiro;
 import com.leocaliban.jogo.xadrez.xadrez.PecaDeXadrez;
 import com.leocaliban.jogo.xadrez.xadrez.enums.Cor;
@@ -18,6 +19,61 @@ public class Rei extends PecaDeXadrez {
 	@Override
 	public boolean[][] movimentosPermitidos() {
 		boolean[][] matriz = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+
+		Posicao posicaoAux = new Posicao(0, 0);
+
+		// Marcando posições acima
+		posicaoAux.setValores(posicao.getLinha() - 1, posicao.getColuna());
+		if (getTabuleiro().posicaoExiste(posicaoAux) && podeMoverPara(posicaoAux)) {
+			matriz[posicaoAux.getLinha()][posicaoAux.getColuna()] = true;
+		}
+
+		// Marcando posições à esquerda
+		posicaoAux.setValores(posicao.getLinha(), posicao.getColuna() - 1);
+		if (getTabuleiro().posicaoExiste(posicaoAux) && podeMoverPara(posicaoAux)) {
+			matriz[posicaoAux.getLinha()][posicaoAux.getColuna()] = true;
+		}
+
+		// Marcando posições à direita
+		posicaoAux.setValores(posicao.getLinha(), posicao.getColuna() + 1);
+		if (getTabuleiro().posicaoExiste(posicaoAux) && podeMoverPara(posicaoAux)) {
+			matriz[posicaoAux.getLinha()][posicaoAux.getColuna()] = true;
+		}
+
+		// Marcando posições abaixo
+		posicaoAux.setValores(posicao.getLinha() + 1, posicao.getColuna());
+		if (getTabuleiro().posicaoExiste(posicaoAux) && podeMoverPara(posicaoAux)) {
+			matriz[posicaoAux.getLinha()][posicaoAux.getColuna()] = true;
+		}
+
+		// Marcando posições à esquerda/acima
+		posicaoAux.setValores(posicao.getLinha() - 1, posicao.getColuna() - 1);
+		if (getTabuleiro().posicaoExiste(posicaoAux) && podeMoverPara(posicaoAux)) {
+			matriz[posicaoAux.getLinha()][posicaoAux.getColuna()] = true;
+		}
+
+		// Marcando posições à direita/abaixo
+		posicaoAux.setValores(posicao.getLinha() + 1, posicao.getColuna() + 1);
+		if (getTabuleiro().posicaoExiste(posicaoAux) && podeMoverPara(posicaoAux)) {
+			matriz[posicaoAux.getLinha()][posicaoAux.getColuna()] = true;
+		}
+
+		// Marcando posições à esquerda/abaixo
+		posicaoAux.setValores(posicao.getLinha() + 1, posicao.getColuna() - 1);
+		if (getTabuleiro().posicaoExiste(posicaoAux) && podeMoverPara(posicaoAux)) {
+			matriz[posicaoAux.getLinha()][posicaoAux.getColuna()] = true;
+		}
+
+		// Marcando posições à direita/acima
+		posicaoAux.setValores(posicao.getLinha() - 1, posicao.getColuna() + 1);
+		if (getTabuleiro().posicaoExiste(posicaoAux) && podeMoverPara(posicaoAux)) {
+			matriz[posicaoAux.getLinha()][posicaoAux.getColuna()] = true;
+		}
 		return matriz;
+	}
+
+	private boolean podeMoverPara(Posicao posicao) {
+		PecaDeXadrez peca = (PecaDeXadrez) getTabuleiro().buscarPecaPorPosicao(posicao);
+		return peca == null || peca.getCor() != getCor();
 	}
 }
